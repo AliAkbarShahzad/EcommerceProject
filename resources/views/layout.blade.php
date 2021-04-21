6a42c8cfba3d870ea84be127f531577a62d6b63c
Ali Akbar Shahzad <aliakbarshahzad5@gmail.com>
2021-04-21 11:21:43 +0500

    login page done

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+    <title>E-comerce Project</title>
+  </head>
+  <body>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  
+  <style type="text/css">
+    .custom-login
+    {
+      height: 500px;
+      padding-top: 100px;
+    }
+  </style>
+  </body>
+</html>
